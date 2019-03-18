819564338e4268a5d73d01601292651184201822
Mahesh Gajadhar <maheshgajadhar1@gmail.com>
2019-03-18 15:40:10 +0100

    update

--- ios/RNTestLibs.m
+++ ios/RNTestLibs.m
@@ -18,14 +18,14 @@ RCT_EXPORT_METHOD(addEvent2:(NSString *)name location:(NSString *)location)
 {
   RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
+RCT_EXPORT_METHOD(addEvent3:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
 RCT_EXPORT_METHOD(getEvent:(RCTResponseSenderBlock)callback)
 {
   NSString *name = @"Mahesh";
   callback(@[[NSNull null], name]);
-}
-RCT_EXPORT_METHOD(addEvent3:(NSString *)name location:(NSString *)location)
-{
-  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
 
