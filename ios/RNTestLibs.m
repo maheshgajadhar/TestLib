e5ac8f1ce84149dd78d4efe46c59dd74bc2abe69
Mahesh Gajadhar <maheshgajadhar1@gmail.com>
2019-03-19 08:38:31 +0100

    promise

--- ios/RNTestLibs.m
+++ ios/RNTestLibs.m
@@ -18,5 +18,17 @@ RCT_EXPORT_METHOD(addEvent2:(NSString *)name location:(NSString *)location)
 {
   RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
+RCT_REMAP_METHOD(getEvent,
+resolver:(RCTPromiseResolveBlock)resolve
+rejecter:(RCTRejectBlock)reject)
+{
+  NSString *event = @"etentje at den haag";
+  if(event){
+    resolve(event);
+  } else{
+    NSString *error = @"Geen event";
+    reject(error);
+  }
+}
 
 @end
