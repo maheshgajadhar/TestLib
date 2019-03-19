8940276b38c01b7e6ab00ba7d27599a75db79f08
Mahesh Gajadhar <maheshgajadhar1@gmail.com>
2019-03-19 09:43:14 +0100

    promise

--- ios/RNTestLibs.m
+++ ios/RNTestLibs.m
@@ -22,6 +22,10 @@ RCT_EXPORT_METHOD(addEvent3:(NSString *)name location:(NSString *)location)
 {
   RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
+RCT_EXPORT_METHOD(addEvent4:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
 RCT_EXPORT_METHOD(getEvent:
 resolver:(RCTPromiseResolveBlock)resolve
 rejecter:(RCTPromiseRejectBlock)reject)
